bb77a2541770c65979ea05a51f5abb9293ee3122
Tiến Đạt <bugslayer.dev@gmail.com>
2022-07-11 20:35:47 +0700

    add bufferline (tabs)

--- Neovim/.config/nvim/lua/core/autocommands.lua
+++ Neovim/.config/nvim/lua/core/autocommands.lua
@@ -2,6 +2,14 @@ vim.api.nvim_create_autocmd({ "User" }, {
   pattern = { "AlphaReady" },
   callback = function()
     vim.cmd [[
+      set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
+    ]]
+  end,
+})
+vim.api.nvim_create_autocmd({ "User" }, {
+  pattern = { "AlphaReady" },
+  callback = function()
+    vim.cmd [[
       set laststatus=0 | autocmd BufUnload <buffer> set laststatus=3
     ]]
   end,
